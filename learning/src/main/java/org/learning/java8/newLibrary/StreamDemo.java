a5cd8b3d06d229c37c981aed4d945cac4a8e8722
hellodaming <824465528@qq.com>
2018-11-13 22:32:03 +0800

    stream转换方法

--- learning/src/main/java/org/learning/java8/newLibrary/StreamDemo.java
+++ learning/src/main/java/org/learning/java8/newLibrary/StreamDemo.java
@@ -14,14 +14,25 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
+import java.util.Set;
 import java.util.Spliterator;
+import java.util.Stack;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+//Java 8 中的 Stream 是对集合（Collection）对象功能的增强，
+//它专注于对集合对象进行各种非常便利、高效的聚合操作（aggregate operation），或者大批量数据操作 (bulk data operation)。
+//Stream API 借助于同样新出现的 Lambda 表达式，极大的提高编程效率和程序可读性。
+//同时它提供串行和并行两种模式进行汇聚操作，并发模式能够充分利用多核处理器的优势，使用 fork/join 并行方式来拆分任务和加速处理过程。
+
+
+//流的操作类型: 1) Intermediate 惰性化的（lazy） 2) Terminal
+//参考https://www.ibm.com/developerworks/cn/java/j-lo-java8streamapi/index.html
 //参考https://colobu.com/2016/03/02/Java-Stream/#%E4%BB%8B%E7%BB%8D
 public class StreamDemo {
 
@@ -72,6 +83,21 @@ public class StreamDemo {
 		// 还有其他很多方法……
 	}
 
+	public void streamTransform(){
+		Stream<String> s =  Arrays.asList("0", "1", "2", "3", "4", "5").stream();
+		
+		// 一个流只能用一次
+		//String[] a =  s.toArray(String[]::new);
+		//List<String> l =  s.collect(Collectors.toList());
+		//Set set = s.collect(Collectors.toSet()); 
+		//Stack<String> stack = s.collect(Collectors.toCollection(Stack::new));
+		
+		String string = s.collect(Collectors.joining());
+		System.out.println(string);
+		
+
+	}
+	
 	public void useStream() {
 
 		Stream<String> s = Arrays.asList("0", "1", "2", "3", "4", "0").stream();
@@ -81,20 +107,19 @@ public class StreamDemo {
 		System.out.println(result.get());
 
 		String str = "Java 8 is a revolutionary release of the world’s #1 development platform. It includes a huge upgrade to the Java programming model and a coordinated evolution of the JVM, Java language, and libraries. Java 8 includes features for productivity, ease of use, improved polyglot programming, security and improved performance. Welcome to the latest iteration of the largest, open, standards-based, community-driven platform.";
-		List<String> l = Arrays.stream(str.split("[.]")).
-				flatMap(line -> Arrays.stream(line.split(" "))).
-				map(x -> {
+		List<String> l = Arrays.stream(str.split("[.]")).flatMap(line -> Arrays.stream(line.split(" ")))
+				.map(x -> {
 					if (x.endsWith(".") || x.endsWith(",")) {
 						return x.substring(0, x.length() - 1).trim();
 					} else {
 						return x.trim();
 					}
-					}).
-				distinct().
-				sorted().
-				peek(System.out::print).
-				limit(15).
-				collect(Collectors.toList());
+				})
+				.distinct()  // 去重
+				.sorted() // 排序
+				.peek(System.out::print) // peek方法方法会使用一个Consumer消费流中的元素，但是返回的流还是包含原来的流中的元素
+				.limit(15) // 支取前面n个
+				.collect(Collectors.toList()); // 
 		System.out.println("");
 		l.forEach(x -> System.out.print(x + " "));
 
@@ -103,7 +128,8 @@ public class StreamDemo {
 	public static void main(String[] args) {
 		Supplier<StreamDemo> demo = StreamDemo::new;
 		demo.get().createStream();
-		demo.get().useStream();
+		demo.get().streamTransform();
+		//demo.get().useStream();
 	}
 
 }
